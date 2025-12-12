87ef13b4692c6fefd1defd9a8c9b160afa273bce
Alberto Carrizo <ae_carrizo@hotmail.com>
2025-12-12 19:42:57 -0300

    Upgrade por Vulnerabilidades

--- components/WhatsappOrPedidosya.jsx
+++ components/WhatsappOrPedidosya.jsx
@@ -7,14 +7,13 @@ export default function WhatsappOrPedidosya({ menu }) {
   const [showMenu, setShowMenu] = useState(false);
   return (
     <>
-   
-        <p className="md:text-4xl text-xl md:mb-2 mb-0">
+      <p className="md:text-4xl text-xl md:mb-2 mb-0">
         Horarios : de Lunes a Sábados
       </p>
       <p className="md:text-3xl text-xl md:mb-8 mb-4 ">
         de 12hs a 14hs y de 20hs a 23hs
       </p>
-      <StoreAddCartButton menu={menu} name={'null'} />
+      <StoreAddCartButton menu={menu} name={"null"} />
     </>
   );
 }
